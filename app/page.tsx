1014be82aff4d10584d0761d28db734bec34510c
Venu Pagadala <venugopalreddypagadala@gmail.com>
2025-10-07 23:02:06 -0400

    added form validations with ZOD

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold mb-8">Welcome to the Home Page</h1>
-      <p className="text-lg">This is a sample Next.js application.</p>
+      <p className="text-lg">This is a sample Next.js application and here I'm exploring shadcn form components with ZOD validation.</p>
       {/* <SampleForm /> */}
       <ComplexForm />
     </main>
